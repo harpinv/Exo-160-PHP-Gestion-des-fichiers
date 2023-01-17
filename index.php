a4c2fc3d69f3c90bee58414fe05116d72b7f0c11
vince <vincent.harpin@orange.fr>
2023-01-17 13:49:44 +0100

    exercice 160

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
+$text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias, aliquam aliquid beatae cum ducimus earum eius impedit mollitia necessitatibus nostrum porro provident quia quibusdam quisquam sapiente vel vero voluptatem!";
 
-
+$first = fopen('monText.txt', 'a+b');
+fwrite($first, $text . "\n");
+fclose($first);
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
  * 4. Faites en sorte d'ajouter chaque chaîne de caractère de ce tableau au fichier créé dans la première partie ( point 2 )
@@ -16,19 +19,41 @@
  */
 // TODO Votre code ici
 
+$table = ["Lorem ipsum dolor sit amet,", "consectetur adipisicing elit.", "Ad alias, aliquam aliquid beatae", "cum ducimus earum eius impedit", "mollitia necessitatibus nostrum", "porro provident quia quibusdam", "quisquam sapiente vel vero voluptatem!", "Lorem ipsum dolor sit amet,", "consectetur adipisicing elit.", "Ad alias, aliquam aliquid beatae"];
+
+$nouveau = fopen('monText.txt', 'c+b');
+
+foreach($table as $element) {
+    fwrite($nouveau, $element . "\n");
+}
+
+fclose($nouveau);
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
+$file = fopen('monText.txt', 'rb');
 
+while($ligne = fgets($file)) {
+    echo "<div> $ligne </div>";
+}
 
+fclose($file);
+
+echo "<br><br>";
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
+if(file_exists('toto.txt')) {
+    echo "Le fichier existe";
+} else {
+    echo "Le fichier n'existe pas";
+}
 
+echo "<br><br>";
 
 /**
  * Super bonus.
@@ -36,3 +61,16 @@
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+$fule = fopen('monText.txt', 'rb');
+
+while($char = fgetc($fule)) {
+    if($char === "a") {
+        echo "<p> @ </p>";
+    } else {
+        echo "<p> $char </p>";
+    }
+}
+
+fclose($fule);
+
+
